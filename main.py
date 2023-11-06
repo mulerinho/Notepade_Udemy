70bf18aecb6d822d4a25b7d505809a1c4880de7f
mulerinho <alan_mulero@hotmail.com>
2023-11-06 09:30:12 -0300

    Finalizando os trabalhos com as funções colocadas.

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ def Open():
     guardando = arquivo.read()
     text_area.insert(1.0, guardando)
 
+# Função para o Ok
+def atualizaFont():
+    size = spin_size.get()
+    font = spin_font.get()
+    text_area.config(font='{} {}'.format(font, size))
 
 
 # Instanciando
@@ -28,6 +33,23 @@ window.title('JujubaPad')
 # Tamanho da janela
 window.minsize(600, 500)
 
+# Criando barra superior
+frame = tkinter.Frame(window, height=30)
+frame.pack(fill='x')
+# Inserindo texto na frame.
+font_text = tkinter.Label(frame,text=' Font: ')
+font_text.pack(side='left')
+spin_font = tkinter.Spinbox(frame, values=('Arial','Verdana'))
+spin_font.pack(side='left')
+# Configurando tamanho da font.
+font_size = tkinter.Label(frame, text='  Font size: ')
+font_size.pack(side='left')
+spin_size = tkinter.Spinbox(frame, from_=0,to=20)
+spin_size.pack(side='left')
+
+# Criando um botão
+button_update = tkinter.Button(frame, text='OK', command=atualizaFont)
+button_update.pack(side='left')
 # Area de texto
 text_area = tkinter.Text(window, width=600, height=500)
 text_area.pack()
